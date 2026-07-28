622d7f5bd9b09ef5b2ecc02ffdf7e2a61ebed6ba
opeach2v <opeach2v@naver.com>
2026-07-29 01:45:32 +0900

    fix: WeatherCard의 key 오류 수정

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -4,7 +4,6 @@ import { CITIES } from "@/app/lib/city";
 import { getCurrentWeather, getDailyWeather } from "../lib/weather";
 
 import WeatherCard from "../components/weatherCard";
-import { BsList } from "react-icons/bs";
 import WeatherHeader from "../components/weatherHeader";
 
 export default async function WeatherPage() {
@@ -24,7 +23,7 @@ export default async function WeatherPage() {
             <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
                 {CITIES.map((city, index) => (
                     <WeatherCard
-                        key={city.name}
+                        key={city.id}
                         city={city}
                         current={currentWeather[index]}
                         daily={dailyWeather[index]}
