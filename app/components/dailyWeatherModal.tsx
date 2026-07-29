30ee852bdf9f0ed411b9b6d494c9e2d4d01e3197
opeach2v <opeach2v@naver.com>
2026-07-29 23:25:53 +0900

    fix: 모달 높이 조정

--- app/components/dailyWeatherModal.tsx
+++ app/components/dailyWeatherModal.tsx
@@ -31,30 +31,19 @@ export default function DailyWeatherModal({
             onClick={onClose}
         >
             <div
-                className="bg-white dark:bg-[#272727] rounded-xl flex flex-col h-[80vh] p-8 w-100 md:w-160 lg:w-200 xl:w-260 shadow-lg"
+                className="bg-white dark:bg-[#272727] rounded-xl h-[80vh] md:h-[55vh] xl:h-[60vh] flex flex-col p-8 w-100 md:w-160 lg:w-200 xl:w-260 shadow-lg"
                 onClick={(e) => e.stopPropagation()}
             >
                 <div className="text-xl font-bold ml-2 mb-2">
                     {cityName} {t.dailyWeather}
                 </div>
-                <div className="flex-1 overflow-y-auto min-h-0">
+                <div className="flex-1 overflow-y-auto min-h-0 h-full">
                     <DailyTable data={dailyRowData} />
                 </div>
 
                 <button
                     onClick={onClose}
-                    className="
-                        mt-5
-                        bg-gray-200
-                        dark:bg-[#3f3f3f]
-                        hover:bg-gray-300
-                        dark:hover:bg-[#4d4d4d]
-                        rounded-md
-                        w-full
-                        py-1
-                        text-lg
-                        font-extrabold
-                    "
+                    className="mt-5 bg-gray-200 dark:bg-[#3f3f3f] hover:bg-gray-300 dark:hover:bg-[#4d4d4d] rounded-md w-full py-1 text-lg font-extrabold"
                 >
                     <p className="text-gray-600 dark:text-gray-300">
                         {t.close}
